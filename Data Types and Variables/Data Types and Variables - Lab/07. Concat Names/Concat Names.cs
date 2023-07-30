af5aa56241280b5ca09dbc420adb95bf802b6f23
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-07-30 23:54:41 +0300

    Add task: Concat Names

---
+++ Data Types and Variables/Data Types and Variables - Lab/07. Concat Names/Concat Names.cs
@@ -0,0 +1,16 @@
+using System.Net.Http.Headers;
+
+namespace _07.ConcatNames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+
+            Console.WriteLine($"{firstName}{delimiter}{lastName}");
+        }
+    }
+}
