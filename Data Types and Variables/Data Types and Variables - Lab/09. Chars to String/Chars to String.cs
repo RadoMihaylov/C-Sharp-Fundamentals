99e446355c60b3385b7e34a50ab30db445375ce7
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-07-31 23:50:26 +0300

    Chars to String

---
+++ Data Types and Variables/Data Types and Variables - Lab/09. Chars to String/Chars to String.cs
@@ -0,0 +1,15 @@
+namespace _09.CharsToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char input1 = char.Parse(Console.ReadLine());
+            char input2 = char.Parse(Console.ReadLine());
+            char input3 = char.Parse(Console.ReadLine());
+
+            Console.Write($"{input1}{input2}{input3}");
+        }
+    }
+}
