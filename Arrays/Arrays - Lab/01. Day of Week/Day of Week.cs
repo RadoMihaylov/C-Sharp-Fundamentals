9f5c66588aca97c87379849cd63b1578faf418e3
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-08-02 23:51:16 +0300

    Add task: Day of Week

---
+++ Arrays/Arrays - Lab/01. Day of Week/Day of Week.cs
@@ -0,0 +1,22 @@
+namespace _01.DayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", " Saturday", "Sunday" };
+
+            int input = int.Parse(Console.ReadLine());
+
+            if (input >= 1 && input<= 7)
+            {
+                string day = days[input-1];
+                Console.WriteLine(day);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
