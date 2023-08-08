0238c68b50e8ee898a681566dc108a2bb1251246
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-08-08 23:55:45 +0300

    Add task: Equal Arrays

---
+++ Arrays/Arrays - Lab/07. Equal Arrays/Equal Arrays.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+
+namespace _7.EqualArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();           
+            int sum = 0;
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                
+                if (arr1[i] != arr2[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                else if (arr1[i] == arr2[i])
+                {              
+                    int arr1Sum = arr1[i];
+                    sum += arr1Sum;                
+                    if (i == arr1.Length - 1)
+                    {
+                       Console.WriteLine($"Arrays are identical. Sum: {sum}");
+                    }
+                }        
+            }
+             
+            
+
+
+        }
+    }
+}
