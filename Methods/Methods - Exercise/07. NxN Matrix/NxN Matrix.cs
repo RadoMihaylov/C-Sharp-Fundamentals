b9fcf9b860752a152aa6d409f54bd2bf9bdc3553
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-10-03 23:49:35 +0300

    Add task: NxN Matrix

---
+++ Methods/Methods - Exercise/07. NxN Matrix/NxN Matrix.cs
@@ -0,0 +1,25 @@
+namespace _07._NxN_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Matrix(n);
+        }
+
+        static void Matrix(int n)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                for (int row = 0; row < n; row++)
+                {
+                    Console.Write(n + " ");
+                    
+                }
+                Console.WriteLine();
+            }
+           
+        }
+    }
+}
