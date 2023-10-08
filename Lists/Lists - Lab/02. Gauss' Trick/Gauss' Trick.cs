600395440b70e770df6311424ea4ba7c4849e793
Radoslav Mihaylov <124840636+RadoMihaylov@users.noreply.github.com>
2023-10-08 23:58:50 +0300

    Add exercise: Gauss' Trick

---
+++ Lists/Lists - Lab/02. Gauss' Trick/Gauss' Trick.cs
@@ -0,0 +1,18 @@
+namespace _02._Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();  
+
+            int originalCount = numbers.Count;
+            for (int i = 0; i < originalCount / 2; i++)
+            {
+                numbers[i] += numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
